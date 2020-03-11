4ae8f2c0b480fe27f63232e1e830d911d8c6172c
Neykem <33416493+Neykem@users.noreply.github.com>
2020-03-12 00:08:51 +0300

    Lab#3 1.0
    Relese Lab#3 SystemProgramming

--- SystemProgramming/SystemProgramming/Processor.h
+++ SystemProgramming/SystemProgramming/Processor.h
@@ -3,12 +3,64 @@ class Processor
 {
 public:
 	Processor();
-	int turn_pro();
+	std::string turn_pro()
 	{
-		int iterator;
-		std::cout << "Write number command" << std::endl;
-		std::cin >> iterator;
-		return 0;
+		try {
+			int iterator = 0;
+			std::string command_line;
+			std::cout << "Write number command (Example: 11232)" << std::endl; 
+			std::cin >> command_line;
+			while (iterator < command_line.length())
+			{
+				if (command_line[iterator] == '1')
+				{
+					std::cout << "Command # 1 - Enter numbers from addition!" << std::endl;
+					int a, b;
+					std::cout << "Enter number 1 : ";
+					std::cin >> a;
+					std::cout << std::endl;
+					std::cout << "Enter number 2 : ";
+					std::cin >> b;
+					std::cout << pr_addition(a, b) << std::endl;
+				}
+				else if (command_line[iterator] == '2')
+				{
+					std::cout << "Command # 2 - Enter string from inversion: ";
+					std::string number_inv;
+					std::cin >> number_inv;
+					std::cout << std::endl;
+					std::cout << pr_inversion(number_inv) << std::endl;
+				}
+				else if (command_line[iterator] == '3')
+				{
+					std::cout << "Command # 3 - Enter number from mixing register: ";
+					int number_in_reg;
+					bool side;
+					std::cin >> number_in_reg;
+					std::cout << std::endl;
+					std::cout << "Enter side from mixing register (side - '1 or after num = left' or '0 = right'): ";
+					std::cin >> side;
+					std::cout << pr_mixing_register(number_in_reg, side) << std::endl;
+				}
+				else
+				{
+					throw 1;
+				}
+				iterator++;
+			}
+		}
+		catch (int error_id)
+		{
+			if (error_id == 1)
+			{
+				return " Error 1: Command number invalid!";
+			}
+			if (error_id != 1)
+			{
+				return " Error 2: Operation failed!";
+			}
+		}
+		return "All command success!";
 	};
 	int pr_addition(int number_1, int number_2)
 	{
@@ -16,60 +68,38 @@ public:
 		buff = number_1 + number_2;
 		return (buff);
 	};
-	int pr_inversion(std::string number_inv)
+	std::string pr_inversion(std::string number_inv)
 	{
-		int count, index, res = 0;
+		int count, index = 0, res = 0;
 		try
 		{
-			for (index = 1; index < number_inv.length; index++)
+			while (index < number_inv.length())
 			{
-				if ((number_inv[index] != 1) || (number_inv[index] != 0))
+				if (number_inv[index] == '1')
 				{
-					count = 1;
-					std::cout << "Число введено неверно. Повторите ввод." << std::endl;
-					return 0;
-					break;
+					number_inv[index] = '0';
 				}
-				int  number_inv_i = (int)(number_inv);
-				while (number_inv)
+				else if (number_inv[index] == '0')
 				{
-					res = res * 10 + number_inv % 10;
-					num /= 10;
-				}
-				
-			}
-
-			while (index < count )
-			{
-				if (number_inv != 0 && number_inv != 1)
-				{
-					throw 1;
-				}
-				if (number_inv == 1)
-				{
-					number_inv = number_inv - 1;
-					return (number_inv);
-				}
-				else if (number_inv == 0)
-				{
-					number_inv = number_inv + 1;
-					return (number_inv);
+					number_inv[index] = '1';
 				}
 				else
 				{
-					throw 2;
+					throw 1;
 				}
+				index++;
 			}
+			return number_inv;
 		}
 		catch (int error_id)
 		{
 			if (error_id == 1)
 			{
-				printf("%d ", "Error 1: Number not binar!");
+				return " Error 1: Number not binar!";
 			}
-			if (error_id == 2)
+			if (error_id != 1)
 			{
-				printf("%d ", "Error 2: Operation failed!");
+				return " Error 2: Operation failed!";
 			}
 		}
 	}
@@ -92,7 +122,7 @@ public:
 		}
 		catch (int error_id)
 		{
-			printf("%d", "Error 1: Failed to shift bit");
+			std::cout << "Error 1: Failed to shift bit" << std::endl;
 		}
 	}
 	~Processor();
--- SystemProgramming/SystemProgramming/SystemProgramming.cpp
+++ SystemProgramming/SystemProgramming/SystemProgramming.cpp
@@ -3,10 +3,12 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	std::string f = "10121";
 	Processor intel_hu_ta = Processor();
-	std::cout << intel_hu_ta.pr_addition(2, 3) << std::endl;
-	std::cout << intel_hu_ta.pr_inversion(0) << std::endl;
-	std::cout << intel_hu_ta.pr_mixing_register(12, 0) << std::endl;
+	std::cout << intel_hu_ta.turn_pro() << std::endl;
+	//std::cout << intel_hu_ta.pr_addition(2, 3) << std::endl;
+	//std::cout << intel_hu_ta.pr_inversion(f) << std::endl;
+	//std::cout << intel_hu_ta.pr_mixing_register(12, 5) << std::endl;
 	getchar();
 	return 0;
 }
